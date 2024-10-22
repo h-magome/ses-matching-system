5b2b9ca9de523b586df30fc7b084dee202967651
h-magome <h-magome@ko-partners.biz>
2024-10-22 14:32:59 +0900

    ビルドエラーを解決する

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -34,6 +34,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/engineers/[id].js"
+    ],
+    "/projects": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/projects.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7da1da58ac2a9305")
+/******/ 		__webpack_require__.h = () => ("e5c7f250bb15ba8a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
